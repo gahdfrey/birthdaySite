12108f79cd8d96b05c3a159f61c824dbe637b52a
gahdfrey <89304019+gahdfrey@users.noreply.github.com>
2025-03-11 17:38:01 +0100

    fixed build issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,24 +3,24 @@ import Footer from "@/components/Footer";
 // import Nav from "@/components/Nav";
 import Slider from "@/components/Slider";
 import VaraText from "@/components/VaraText";
-import NewSlider from "@/components/NewSlider";
 
 export default function Home() {
   return (
     <main
-      className={` font-cursive relative p-4 max-w-[1170px] mx-auto min-h-screen bg-cover bg-center bg-no-repeat`}
+      className={` font-cursive relative p-4  min-h-screen bg-cover bg-center bg-no-repeat`}
       style={{ backgroundImage: "url('/IMG_1985.JPG')" }}
     >
-      {/* Overlay div */}
-      <div className="absolute inset-0 bg-black opacity-10"></div>
+      <div className="max-w-[1170px] mx-auto">
+        {/* Overlay div */}
+        <div className="absolute inset-0 bg-black opacity-10"></div>
 
-      {/* Content (ensuring it's above the overlay) */}
-      <div className="relative z-10">
-        {/* <Nav /> */}
-        <VaraText text={""} />
-        <Slider />
-        {/* <NewSlider /> */}
-        <Footer />
+        {/* Content (ensuring it's above the overlay) */}
+        <div className="relative z-10">
+          {/* <Nav /> */}
+          <VaraText text={""} />
+          <Slider />
+          <Footer />
+        </div>
       </div>
     </main>
   );
--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,117 +1,3 @@
-// "use client";
-
-// import * as React from "react";
-// import { motion } from "framer-motion";
-// import Image from "next/image";
-
-// const carouselItems = [
-//   { content: "/IMG_1978.jpg" },
-//   { content: "/IMG_1979.jpg" },
-//   { content: "/IMG_1980.jpg" },
-//   { content: "/IMG_1981.jpg" },
-//   { content: "/IMG_1983.jpg" },
-//   { content: "/IMG_1984.jpg" },
-//   { content: "/IMG_2007.jpg" },
-//   { content: "/IMG_2006.jpg" },
-//   { content: "/IMG_2005.jpg" },
-//   { content: "/IMG_2004.jpg" },
-//   { content: "/IMG_2003.jpg" },
-//   { content: "/IMG_2002.jpg" },
-//   { content: "/IMG_2001.jpg" },
-//   { content: "/IMG_1999.jpg" },
-// ];
-
-// const marqueeVariants = {
-//   animateLeft: {
-//     x: ["0%", "-100%"],
-//     transition: {
-//       x: {
-//         repeat: Infinity,
-//         repeatType: "loop",
-//         duration: 50,
-//         ease: "linear",
-//       },
-//     },
-//   },
-//   animateRight: {
-//     x: ["-100%", "0%"],
-//     transition: {
-//       x: {
-//         repeat: Infinity,
-//         repeatType: "loop",
-//         duration: 50,
-//         ease: "linear",
-//       },
-//     },
-//   },
-// };
-
-// const ImageCarousel = () => {
-//   const topRow = carouselItems.slice(0, 7);
-//   const bottomRow = carouselItems.slice(7, 14);
-
-//   return (
-//     <section className="z-20 overflow-hidden w-full">
-//       <h2 className="text-5xl font-bold text-center text-gray-900 dark:text-white my-8">
-//         Photos of Juliet
-//       </h2>
-//       <div className="relative w-full h-[50rem] overflow-hidden">
-//         {/* Top Row (Moving Left) */}
-//         <motion.div
-//           className="flex"
-//           style={{ position: "absolute", top: "0" }}
-//           variants={marqueeVariants}
-//           animate="animateLeft"
-//         >
-//           {[...topRow, ...topRow].map((item, index) => (
-//             <motion.div
-//               key={index}
-//               className="w-[300px] h-[22rem] mx-2 rounded-lg overflow-hidden shadow-md"
-//             >
-//               <Image
-//                 src={item.content}
-//                 alt={`Image ${index + 1}`}
-//                 width={500}
-//                 height={400}
-//                 className="w-full h-full object-cover"
-//               />
-//             </motion.div>
-//           ))}
-//         </motion.div>
-
-//         {/* Bottom Row (Moving Right) */}
-//         <motion.div
-//           className="flex"
-//           style={{
-//             position: "absolute",
-//             top: "50%",
-//             transform: "translateY(50px)",
-//           }}
-//           variants={marqueeVariants}
-//           animate="animateRight"
-//         >
-//           {[...bottomRow, ...bottomRow].map((item, index) => (
-//             <motion.div
-//               key={index}
-//               className="w-[300px] h-[22rem] mx-2 rounded-lg overflow-hidden shadow-md"
-//             >
-//               <Image
-//                 src={item.content}
-//                 alt={`Image ${index + 1}`}
-//                 width={500}
-//                 height={400}
-//                 className="w-full h-full object-cover"
-//               />
-//             </motion.div>
-//           ))}
-//         </motion.div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default ImageCarousel;
-
 "use client";
 
 import * as React from "react";
@@ -119,7 +5,8 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import Confetti from "react-confetti";
 import { useState, useEffect } from "react";
-import { useWindowSize } from "react-use"; // Helps get the screen width & height
+import { useWindowSize } from "react-use";
+// Helps get the screen width & height
 
 const carouselItems = [
   { content: "/IMG_1978.jpg" },
@@ -166,23 +53,32 @@ const marqueeVariants = {
 const ImageCarousel = () => {
   const topRow = carouselItems.slice(0, 7);
   const bottomRow = carouselItems.slice(7, 14);
+  // const [showConfetti, setShowConfetti] = useState(true);
   const { width, height } = useWindowSize();
   const [showConfetti, setShowConfetti] = useState(true);
+  const [isClient, setIsClient] = useState(false);
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => setShowConfetti(false), 30000); // Show confetti for 4 seconds
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   useEffect(() => {
-    const timer = setTimeout(() => setShowConfetti(false), 30000); // Show confetti for 4 seconds
+    setIsClient(true);
+    const timer = setTimeout(() => setShowConfetti(false), 30000);
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <section className="z-20 overflow-hidden w-full">
-      {showConfetti && (
+      {/* {showConfetti && ( */}
+      {isClient && showConfetti && (
         <div className="fixed top-0 left-0 z-1000 flex items-center justify-center">
           <Confetti
             width={window.innerWidth || 300}
             height={window.innerHeight || 300}
-            numberOfPieces={250}
-            initialVelocityY={10}
+            numberOfPieces={30}
+            initialVelocityY={50}
             wind={0.05}
             gravity={0.5}
           />
